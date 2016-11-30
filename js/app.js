3673ae15b8519ed84e6ddb7b8a83f6d98296b5bd
epdsn <ibestudios@gmail.com>
2016-11-30 01:33:20 -0700

    style updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 $(document).foundation()
+
+
+$(function(){
+
+    $( ".btnClickToAbout" ).click(function() {
+        $('#abouteric').ScrollTo({
+            duration: 2000,
+            durationMode: 'all'
+        });
+    });
+
+
+    $( ".btnClickToContact" ).click(function() {
+        $('#contactform').ScrollTo({
+            duration: 2000,
+            durationMode: 'all'
+        });
+
+    });
+
+    $( ".btnClickToWork" ).click(function() {
+        $('#work').ScrollTo({
+            duration: 2000,
+            durationMode: 'all'
+        });
+    });
+
+});
